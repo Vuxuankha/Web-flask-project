c7cbdad16c432f2bde80b55399d8335b55432805
Vuxuankha <102339314+Vuxuankha@users.noreply.github.com>
2023-06-20 09:28:39 +0000

    db_connect

--- app.py
+++ app.py
@@ -1,36 +1,13 @@
 from flask import Flask, render_template, jsonify
+from database import load_jobs_fromdb
 
 app = Flask(__name__)
-JOBS = [
-  {
-    'id': 1,
-    'title': 'Data analyst',
-    'location': 'Benaluru,India',
-    'salary': 'Rs.10000'
-  },
-  {
-    'id': 2,
-    'title': 'Data scrien',
-    'location': 'Deihl,India',
-    'salary': 'Rs.150000'
-  },
-  {
-    'id': 3,
-    'title': 'App website',
-    'location': 'Remote',
-    'salary': 'Rs.20000'
-  },
-]
 
 
 @app.route('/')
 def hello_world():
-  return render_template('home.html', job=JOBS, company_name='kha')
-
-
-@app.route('/jobs')
-def job_list():
-  return jsonify(JOBS)
+  jobs = load_jobs_fromdb()
+  return render_template('home.html', job=jobs, company_name='kha')
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -1,1 +1,18 @@
-import sql
+from sqlalchemy import create_engine, text
+
+db_connect = "mysql+pymysql://iierkby7ym1jeib5rqqb:pscale_pw_PrGPJcxAGPoUzdxDBQ3gkZ2iH4uEDDMUgGig9qjJPxX@aws.connect.psdb.cloud/websiteflask?charset=utf8mb4"
+engine = create_engine(db_connect,
+                       connect_args={"ssl": {
+                         "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
+
+
+def load_jobs_fromdb():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))
+    return_all = result.all()
+    jobs = []
+    for row in return_all:
+      # Thay 'column1', 'column2' bằng tên các cột thực tế
+      jobs.append(row)
+    return jobs
